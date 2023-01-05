e431d8caba16139b07bb32a7135caa7adfffac2d
songhashin <lang_00@naver.com>
2023-01-06 02:18:27 +0900

    [querydsl] offset 단점 기술, 서브쿼리 대체 기술

--- src/main/java/me/songha/tutorial/repository/FinalMemberRepository.java
+++ src/main/java/me/songha/tutorial/repository/FinalMemberRepository.java
@@ -3,13 +3,18 @@ package me.songha.tutorial.repository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import me.songha.tutorial.domain.Member;
 import me.songha.tutorial.dto.*;
 import me.songha.tutorial.repository.support.OrderByNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static me.songha.tutorial.domain.QMember.member;
@@ -231,6 +236,96 @@ public class FinalMemberRepository {
                 .fetchFirst();
 
         return fetchOne != null;
+    }
+
+    /** ============================================================================================================ */
+
+    /**
+     * offset 을 이용하면 offset + limit 만큼의 데이터를 읽어야 한다.
+     * 이와 같은 형태는 페이지 번호가 뒤로 갈수록 앞에서 읽었던 행을 다시 읽어야 한다.
+     * 이 말은 offset이 10000이고 limit가 20이라면 10,020 행을 읽어야 한다는 것이고 그러고 나서 10,000 개의 행을 버리는 것이다.
+     * 그렇기 때문에 성능 상에 안좋다는 점인데 No Offset 방식은 시작 지점을 인덱스로 빠르게 찾아 첫 페이지부터 읽도록 하는 방식이다.
+     */
+    public Page<MemberTeamDto> search(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Member> countQuery = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+    }
+
+    /**
+     * 이전에 조회된 결과를 한번에 건너뛸 수 있게 마지막 조회 결과의 ID 를 조건문에 사용하는 방식을 이용한다.
+     */
+    public List<MemberDto> noOffset(Long lastMemberId, int limit) {
+        return queryFactory.select(
+                        new QMemberDto(
+                                member.username,
+                                member.age))
+                .from(member)
+                .where(member.username.contains("member")
+                        .and(memberIdLt(lastMemberId)))
+                .orderBy(member.id.desc())
+                .limit(limit)
+                .fetch();
+    }
+
+    private BooleanExpression memberIdLt(Long lastMemberId) {
+        return lastMemberId != null ? member.id.lt(lastMemberId) : null;
+    }
+
+    /** ============================================================================================================ */
+
+    /**
+     * querydsl 에서는 from 절에서의 서브쿼리를 지원하지 않는다.
+     * 그렇기 때문에 이를 우회하여 조회하는 방법으로
+     * 우선 첫번째 select로 대상이 되는 pk 리스트를 조회한 다음,
+     * 두번째 select 에서 in 조건으로 검색하는 것이다.
+     */
+    public List<MemberDto> useCoveringIndex(int offset, int limit) {
+        List<Long> ids = queryFactory
+                .select(member.id)
+                .from(member)
+                .where(member.username.like("member%"))
+                .orderBy(member.id.desc())
+                .limit(limit)
+                .offset(offset)
+                .fetch();
+
+        if (ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+                .select(new QMemberDto(
+                        member.username,
+                        member.age
+                ))
+                .from(member)
+                .where(member.id.in(ids))
+                .orderBy(member.id.desc())
+                .fetch();
     }
 
 }
