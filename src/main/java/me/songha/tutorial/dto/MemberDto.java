6a2d91db46ce199bfa20703645772280c6862263
songhashin <lang_00@naver.com>
2023-01-20 18:25:49 +0900

    [querydsl] 주석 추가

--- src/main/java/me/songha/tutorial/dto/MemberDto.java
+++ src/main/java/me/songha/tutorial/dto/MemberDto.java
@@ -10,6 +10,9 @@ public class MemberDto {
     private String username;
     private int age;
 
+    /**
+     * QueryProjection 를 붙이면 dto 에서도 QClass 를 만든다.
+     */
     @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
